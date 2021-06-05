9b74020622b5735331627aa26da29111e520687e
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-05 11:02:18 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -8,25 +8,25 @@ int main() {
 
 	do {                                        //Loop till the user choices to Quit
 		std::cout << "> Press 1 for checking the balance" << std::endl;
-		std::cout << "> Press 2 to deposit" << std::endl;
+		std::cout << "> Press 2 to deposit" << std::endl; //console output
 		std::cout << "> Press 3 to withdrew" << std::endl;
 		std::cout << "> Press 4 to QUIT" << std::endl;
 		std::cout << "> Enter your selection";
-		std::cin >> choice;
-		switch (choice) {
+		std::cin >> choice; //get input from the user
+		switch (choice) {   //switching choices
 		case '1':
 			std::cout << balance << "$\n";
 			break;
 		case '2':
 			std::cout << "how much you want to depos ?\n";
-			std::cin >> amount;
-			balance = +amount;
+			std::cin >> amount; //geting input from user
+			balance = +amount;  //increases the balance
 			break;
 		case '3':
 			std::cout << "how muce you want to withdrew >\n";
 			std::cin >> amount;
-			if (balance < amount)
-				std::cout << "inseficent money!\n";
+			if (balance < amount)//if the money in the balance is less then the required
+				std::cout << "inseficent money!\n"; //console output
 			else
 				std::cout << "done!\n";
 			balance -= amount;
