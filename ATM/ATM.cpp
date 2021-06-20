b24b6a5ee76832fb451794adadb2598b10c6c247
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-06-20 23:41:05 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -19,8 +19,8 @@ int main() {
 			break;
 		case '2':
 			std::cout << "how much you want to depos ?\n";
-			std::cin >> amount; //geting input from user
-			balance = +amount;  //increases the balance
+			std::cin >> amount;//geting input from user
+			balance = +amount;//increases the balance
 			break;
 		case '3':
 			std::cout << "how muce you want to withdrew >\n";
@@ -29,7 +29,7 @@ int main() {
 				std::cout << "inseficent money!\n"; //console output
 			else
 				std::cout << "done!\n";
-			balance -= amount; //decreases the balance
+			balance -= amount;//decreases the balance
 			break;
 		case '4':
 			std::cout << "See ya ^_^";
@@ -38,7 +38,7 @@ int main() {
 			std::cout << "invalid selection, try agin\n";
 			break;
 		}
-	} while (choice != '4'); //when the user inputs '4' the program ends
+	} while (choice != '4');//when the user inputs '4' the program ends
 	return 0;
 
 }
