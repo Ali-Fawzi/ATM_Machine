0c89ae7e0c8f0e3966bdcc2d36b3bfdaa06e0fb9
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-07-31 21:53:04 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -4,7 +4,7 @@ int main() {
 	system("color 2");//set the screen colour to green.
 	double amount, balance = 0.00;//used vaule in the program.
 	char choice;//user choices.
-	std::cout << "\t ATM MACHINE" << std::endl;//console output
+	std::cout << "\t ATM MACHINE" << std::endl;//console output.
 
 	do {//Loop till the user choices to Quit
 		std::cout << "> Press 1 for checking the balance" << std::endl;
