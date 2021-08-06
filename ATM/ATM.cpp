1e7b1a796a2cf7b0ba715a0b34408a4f51bf6155
kenpachi01011 <83194640+kenpachi01011@users.noreply.github.com>
2021-08-06 20:53:11 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -19,7 +19,7 @@ int main() {
 			break;
 		case '2':
 			std::cout << "how much you want to depos ?\n";
-			std::cin >> amount;//geting input from user
+			std::cin >> amount;//geting input from user.
 			balance = +amount;//increases the balance.
 			break;
 		case '3':
