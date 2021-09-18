dabe40aa89dabc9cf3ab358a89d6ec444dab41f1
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-09-18 16:33:59 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -12,7 +12,7 @@ int main() {
 		std::cout << "> Press 3 to withdrew" << std::endl;
 		std::cout << "> Press 4 to QUIT" << std::endl;
 		std::cout << "> Enter your selection";
-		std::cin >> choice;//get input from the user
+		std::cin >> choice;//get input from the user.
 		switch (choice) {//switching choices
 		case '1':
 			std::cout << balance << "$\n";
