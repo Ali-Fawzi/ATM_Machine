d66153a3cf36a854884f45be0c8e0b1651486634
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-10-07 18:55:22 +0300

    Update ATM.cpp

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<windows.h>
 int main() {
-	system("color 2");//set the screen colour to green
-	double amount, balance = 0.00;//used vaule in the program
+	system("color 2");//set the screen colour to green.
+	double amount, balance = 0.00;//used vaule in the program.
 	char choice;//user choices.
 	std::cout << "\t ATM MACHINE" << std::endl;//console output.
 
